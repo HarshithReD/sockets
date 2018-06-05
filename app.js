3f9ee8b701d52df310c478be85d2b213ec88d694
Harshith <harshithgangannagari@gmail.com>
2018-06-05 13:26:48 +0530

    read,write,append files

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 const fs=require('fs');
-fs.appendFile('message.txt','This is an example',function(err){
-	if (err) throw err;
-	console.log('data has been appended!');
-})
+const os=require('os');
+var a=10;
+const variables=require('./variables.js');
+var command=process.argv[2];
+var para1=process.argv[3];
+var para2=process.argv[4];
+
+if(command==='read'){
+	if(!para1){
+		process.exit(0);
+	}
+	fs.readFile(para1,'utf8',function(err,data){
+		if(err) throw err;
+		console.log(data);
+	});
+
+}else if(command === 'write'){
+	if(!para1 || !para2){
+		process.exit(0);
+	}
+	fs.writeFile(para1,para2,function(err){
+		if(err) throw err;
+		console.log('data has been writen');
+	});
+}
+else if (command === 'append'){
+	if(!para1 || !para2){
+		process.exit(0);
+	}
+	fs.appendFile(para1,para2,function(err){
+		if(err) throw err;
+		console.log('data has been modified');
+	});
+}
+
---
+++ variables.js
@@ -0,0 +1,8 @@
+var a=12;
+var b=123;
+module.exports.a=a;
+module.exports.b=b;
+module.exports.callFunc= ()=>{
+	var c=a+b;
+	return c;
+}
